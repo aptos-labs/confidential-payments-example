311a004a8df0df942fa885ef29c48455d9bd498e
lukachi <lukachisama@gmail.com>
2025-03-27 14:03:28 +0200

    Refactor RootPage layout for improved responsiveness and update typography styles

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,21 +1,20 @@
 import Link from 'next/link'
 
 import { UiButton } from '@/ui/UiButton'
-import UiThreads from '@/ui/UiThreads'
 
 export default async function RootPage() {
   return (
-    <div className='isolate size-full bg-backgroundPrimary'>
-      <div className='absolute inset-0 z-10 size-full'>
+    <div className='isolate size-full max-w-[100vw] overflow-hidden bg-backgroundPrimary'>
+      {/* <div className='absolute inset-0 z-10 size-full'>
         <UiThreads amplitude={1} distance={0} enableMouseInteraction={true} />
-      </div>
+      </div> */}
 
       <div className='absolute left-1/2 top-1/2 z-20 flex -translate-x-1/2 -translate-y-1/2 flex-col items-center justify-center gap-4 text-center'>
-        <span className='tex-textPrimary typography-h1'>
+        <span className='text-textPrimary typography-h3 sm:typography-h2 md:typography-h1'>
           Confidential Assets
         </span>
 
-        <span className='tex-textPrimary typography-subtitle1'>
+        <span className='tex-textPrimary typography-subtitle3 md:typography-subtitle1'>
           Bringing innovative solutions to secure your digital world.
         </span>
 

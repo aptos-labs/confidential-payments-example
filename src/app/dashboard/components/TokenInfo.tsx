b2d6e28d7a4a41ee07a0407a59800a720285f2f6
lukachi <lukachisama@gmail.com>
2025-03-29 00:50:52 +0200

    Refactor TokenInfo component to use useMemo for token icon rendering and improve error handling for invalid icon URLs

--- src/app/dashboard/components/TokenInfo.tsx
+++ src/app/dashboard/components/TokenInfo.tsx
@@ -2,7 +2,7 @@
 
 import Avatar from 'boring-avatars'
 import Image from 'next/image'
-import { HTMLAttributes } from 'react'
+import { HTMLAttributes, useMemo } from 'react'
 
 import { TokenBaseInfo } from '@/store/wallet'
 import { cn } from '@/theme/utils'
@@ -10,17 +10,31 @@ import { cn } from '@/theme/utils'
 export default function TokenInfo({
   token,
 }: { token: TokenBaseInfo } & HTMLAttributes<HTMLDivElement>) {
-  return (
-    <div className='mt-3 flex flex-col gap-3'>
-      {token.iconUri ? (
+  const tokenIconComponent = useMemo(() => {
+    try {
+      const tokenImgUrl = new URL(token.iconUri)
+
+      return token.iconUri ? (
         <Image
-          src={token.iconUri}
+          src={tokenImgUrl.href}
           alt={token.name}
+          width={75}
+          height={75}
           className='size-[75] rounded-full'
         />
       ) : (
         <Avatar name={token.address} size={75} variant='pixel' />
-      )}
+      )
+    } catch (error) {
+      /* empty */
+    }
+
+    return <Avatar name={token.address} size={75} variant='pixel' />
+  }, [token.address, token.iconUri, token.name])
+
+  return (
+    <div className='mt-3 flex flex-col gap-3'>
+      {tokenIconComponent}
       <TokenInfoItem label='Name' value={token.name} />
       <TokenInfoItem label='Symbol' value={token.symbol} />
       <TokenInfoItem label='Decimals' value={String(token.decimals)} />

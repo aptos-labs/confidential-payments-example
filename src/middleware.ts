c3aab43da1f26d82782271489ff92e44130ddf1b
lukachi <lukachisama@gmail.com>
2025-04-10 22:05:40 +0300

    Add MaintenancePage component and implement module validation middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,6 +1,10 @@
 import { createMiddleware, type MiddlewareFunctionProps } from '@rescale/nemo'
 import { NextResponse } from 'next/server'
 
+import { aptos } from './api/modules/aptos'
+import { config as appConfig } from './config'
+import { tryCatch } from './helpers'
+
 // eslint-disable-next-line unused-imports/no-unused-vars
 const appGuard = async ({ request }: MiddlewareFunctionProps) => {
   // const sessionCookie = getSessionCookie(request, {
@@ -55,8 +59,25 @@ const authGuard = async ({ request }: MiddlewareFunctionProps) => {
   return NextResponse.next()
 }
 
+const moduleValidGuard = async ({ request }: MiddlewareFunctionProps) => {
+  const [, err] = await tryCatch(
+    aptos.getAccountModule({
+      accountAddress: appConfig.CONFIDENTIAL_ASSET_MODULE_ADDR,
+      moduleName: 'confidential_asset',
+    }),
+  )
+
+  if (err) {
+    console.error('Error fetching module:', err)
+    return NextResponse.redirect(new URL('/maintenance', request.url))
+  }
+
+  return NextResponse.next()
+}
+
 const middlewares = {
-  '/dashboard': [],
+  '/dashboard': [moduleValidGuard],
+  '/': [moduleValidGuard],
 }
 
 export const middleware = createMiddleware(middlewares)
